395da63c8978e28f0c06a59bc3fd59433a59d538
Sergey <37815724+lwnemesis@users.noreply.github.com>
2023-01-05 21:02:31 -0800

    Tokenservice partially working

--- WebMvc/Program.cs
+++ WebMvc/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using WebMvc.Infrastructure;
 using WebMvc.Services;
+using WebMvc.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
@@ -10,6 +14,41 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IHttpClient, CustomHttpClient>();
 builder.Services.AddTransient<ICatalogService, CatalogService>();
+builder.Services.AddTransient<IIdentityService<ApplicationUser>, IdentityService>();
+
+var identityUrl = configuration["IdentityUrl"];
+var callBackUrl = configuration["CallBackUrl"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.Authority = identityUrl.ToString();
+    options.SignedOutRedirectUri = callBackUrl.ToString();
+    options.ClientId = "mvc";
+    options.ClientSecret = "secret";
+    options.RequireHttpsMetadata = false;
+    options.SaveTokens = true;
+    options.ResponseType = "code id_token";
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("order");
+    options.Scope.Add("basket");
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+
+        NameClaimType = "name",
+        RoleClaimType = "role",
+    };
+});
 
 var app = builder.Build();
 
