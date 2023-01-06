78da2e8b01a947ac968d03cd76417d3e5b838e07
qingliu816 <112495258+qingliu816@users.noreply.github.com>
2023-01-05 23:07:06 -0800

    add WebMvc EventsCatalog.cs add WebMvc EventType.cs add gitignore file

---
+++ WebMvc/Models/EventType.cs
@@ -0,0 +1,16 @@
+﻿namespace WebMvc.Models
+{
+    public class EventType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateTime { get; set; }
+        public string PictureUrl { get; set; }
+        public int CategoryTypeId { get; set; }
+        public int OrganizerTypeId { get; set; }
+        public string CategoryType  { get; set; }
+        public string OrganizerType { get; set; }
+    }
+}
