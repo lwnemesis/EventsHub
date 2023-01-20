5c3552a14124a44718bc772dd01124d328686965
harshithavaas <harshitha.vaas@gmail.com>
2023-01-19 20:33:38 -0800

    All 3a - MVC tasks

---
+++ WebMvc/ViewModels/EventIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebMvc.Models;
+
+namespace WebMvc.ViewModels
+{
+    public class EventIndexViewModel
+    {
+        public IEnumerable<SelectListItem> Organizers { get; set; }
+        public IEnumerable<SelectListItem> Types { get; set; }
+        public IEnumerable<EventType> Events { get; set; }
+        public PaginationInfo PaginationInfo { get; set; }
+        public int? OrganizerFilterApplied { get; set; }
+        public int? TypesFilterApplied { get; set; }
+    }
+}
