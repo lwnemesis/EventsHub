ae82ed7469f94e7ca231d57287afb797adb064ab
qingliu816 <112495258+qingliu816@users.noreply.github.com>
2023-01-26 22:06:01 -0800

    update

--- CartApi/Program.cs
+++ CartApi/Program.cs
@@ -1,13 +1,17 @@
 using CartApi.Data;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using StackExchange.Redis;
+using System.IdentityModel.Tokens.Jwt;
+
+using System.IdentityModel.
 
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddTransient<ICartRepository,RedisCartRepository>();
 builder.Services.AddSingleton<ConnectionMultiplexer>(cm =>
 {
@@ -18,6 +22,50 @@ builder.Services.AddSingleton<ConnectionMultiplexer>(cm =>
     return ConnectionMultiplexer.Connect(config);
 
 });
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+var identityUrl = configuration["IdentityUrl"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = identityUrl.ToString();
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = false,
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+    options.Audience = "basket";
+});
+
+builder.Services.AddMassTransit(cfg =>
+{
+    cfg.AddConsumer<OrderCompletedEventConsumer>();
+    cfg.AddBus(provider =>
+    {
+        return Bus.Factory.CreateUsingRabbitMq(rmq =>
+        {
+            rmq.Host(new Uri("rabbitmq://rabbitmq"), "/", h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            });
+            rmq.ReceiveEndpoint("JewelscartOct2022", e =>
+            {
+                e.ConfigureConsumer<OrderCompletedEventConsumer>(provider);
+
+            });
+        });
+
+    });
+});
+
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
